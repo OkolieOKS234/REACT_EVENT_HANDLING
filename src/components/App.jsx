9bb31bbc7e4a1bea60e43743353832a5362f90e0
user name <98519572+OkolieOKS234@users.noreply.github.com>
2022-09-22 16:04:53 +0100

    created functions with useState, conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [mouseOver, setmouseOver] = useState(false);
+  function headerSetter() {
+    setHeadingText("submitted");
+  }
+
+  function handleMouseOver() {
+    setmouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setmouseOver(false);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={headerSetter}
+        style={{ backgroundColor: mouseOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
